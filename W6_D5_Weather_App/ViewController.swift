a340393970e2f0957531bd800a8105f76e557e47
khalid <alhidan34@gmail.com>
2021-12-12 23:14:49 +0300

    Initial Commit

---
+++ W6_D5_Weather_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W6_D5_Weather_App
+//
+//  Created by Khalid Alhidan on 12/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
